5470e4c0107096b3109644a5b6a5968eaa8bf72b
Valeriya Kozhynova <valko92@hotmail.com>
2018-03-06 11:32:43 -0500

    new building vimeo added

--- assets/js/main.js
+++ assets/js/main.js
@@ -880,7 +880,7 @@ $( document ).ready( function ()
     //VIMEO Modal
     $( '#vimeo' ).on( 'shown.bs.modal', function ()
     {
-        $( '#vimeo' ).find( 'iframe' ).attr( 'src', 'https://player.vimeo.com/video/144034979?autoplay=1' );
+        $( '#vimeo' ).find( 'iframe' ).attr( 'src', 'https://player.vimeo.com/video/258700020?autoplay=1&color=C7C8CA' );
     });
 
     $( '#vimeo' ).on( 'hidden.bs.modal', function ()
